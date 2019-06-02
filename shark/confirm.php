7e4e6febf8e72170ebd75979b84ca23aa24f2ce9
BubbaSWalter <bob.willmann@outlook.com>
2019-06-02 10:24:39 -0500

    Updated Shark Confirmation Page.

--- shark/confirm.php
+++ shark/confirm.php
@@ -1,3 +1,31 @@
+<?php
+	$choice = "No Choice Submitted";
+	$uname = "Guest";
+	if(isset($_COOKIE["username"])) {
+		include '../uname.php';
+		$servername = "localhost";
+		$username = "u919436859_admin";
+		$password = "GjefadD~i63a";
+		$dbname = "u919436859_shark";
+		
+        $conn = new mysqli($servername, $username, $password, $dbname); // Create connection
+		if ($conn->connect_error) {     // Check connection
+			die("Connection failed: " . $conn->connect_error);
+		}
+		
+		$sql = "SELECT * FROM sharkvoting WHERE Username = '$uname'";
+        $result = $conn->query($sql);
+        
+        if ($result->num_rows > 0) {
+		    // output data of each row
+		    while($row = $result->fetch_assoc()) {
+			    $choice = $row["Choice"];
+		    }
+	    }
+	    $conn->close();
+	}
+?>
+
 <!DOCTYPE html>
 <html>
 	<meta charset="UTF-8">
@@ -15,7 +43,7 @@
 	</div>
 	<article id="article">
 		<p>Hello <span id="username">Guest</span>:</p>
-		<p>We have your vote for <span id="challenge">Challenge #0 NULL</span>
+		<p>We have your vote for <span id="challenge"><?php echo $choice;?></span>
 		
 	</article>
 	<script type="text/javascript" src="js/confirm.js"></script>
