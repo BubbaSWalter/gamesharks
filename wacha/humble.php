5d01c97d13302fa7e7bb5b0c4288b8231246ab38
BubbaSWalter <bob.willmann@outlook.com>
2019-06-01 23:31:35 -0500

    Update humble.php

--- wacha/humble.php
+++ wacha/humble.php
@@ -126,6 +126,17 @@ include '../meistercheck.php';
 			$conn->close();
 			?>
 		</div>
+		<div id="winners">
+                <form action="voting/winners.php">
+                    Winning Choices:<br>
+                    First Place: <input type="text" name="first" value="<?php echo $winners['first'] ?>"><br>
+                    Second Place: <input type="text" name="second" value="<?php echo $winners['second'] ?>"><br>
+                    Third Place: <input type="text" name="third" value="<?php echo $winners['third'] ?>"><br>
+                    Fouth Place: <input type="text" name="fourth" value="<?php echo $winners['fourth'] ?>"><br>
+                    Fifth Place: <input type="text" name="fifth" value="<?php echo $winners['fifth'] ?>"><br>
+                    <input type="submit" value="Update Winners">
+                </form>
+        </div>
             <div>
                 
                 <form action="voting/full_wipe.php">
@@ -193,17 +204,7 @@ include '../meistercheck.php';
                     <input type="submit" value="Change Voting Mode">
                 </form>
             </div>
-			<div id="winners">
-                <form action="voting/winners.php">
-                    Winning Choices:<br>
-                    First Place: <input type="text" name="first" value="<?php echo $winners['first'] ?>"><br>
-                    Second Place: <input type="text" name="second" value="<?php echo $winners['second'] ?>"><br>
-                    Third Place: <input type="text" name="third" value="<?php echo $winners['third'] ?>"><br>
-                    Fouth Place: <input type="text" name="fourth" value="<?php echo $winners['fourth'] ?>"><br>
-                    Fifth Place: <input type="text" name="fifth" value="<?php echo $winners['fifth'] ?>"><br>
-                    <input type="submit" value="Update Winners">
-                </form>
-        </div>
+
 	</article>
 	<script type="text/javascript" src="../js/ocean.js"></script>
 </body>
