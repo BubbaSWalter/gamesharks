83afd01885c79a963e36c30f052f547e1a1ceced
BubbaSWalter <bob.willmann@outlook.com>
2019-06-02 09:57:36 -0500

    updating shark voting

--- shark/voting.php
+++ shark/voting.php
@@ -4,29 +4,9 @@
 
 <?php 
 	if(isset($_COOKIE["username"])) {
-		$uname = $_COOKIE["username"];
-		
-		$curl = curl_init();
-		curl_setopt_array($curl, array(
-			CURLOPT_URL => "https://id.twitch.tv/oauth2/validate",
-			CURLOPT_RETURNTRANSFER => true,
-			CURLOPT_TIMEOUT => 30,
-			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-			CURLOPT_CUSTOMREQUEST => "GET",
-			CURLOPT_HTTPHEADER => array(
-				"Authorization: OAuth ". $uname 
-			),
-		));
-		
-		$resp = curl_exec($curl);
-		$response = json_decode($resp, true);
-		
-		$uname = $response['login'];
-		curl_close($curl);
-		
+		include('../uname.php')
+		include('../sharkcheck.php')
 		$check = false;
-		
-
 		
 		$conn = new mysqli($servername, $username, $password, $dbname); // Create connection
 		if ($conn->connect_error) {     // Check connection
