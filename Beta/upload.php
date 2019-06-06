ee4319c2172175a041da0433440df939710d6c8d
BubbaSWalter <bob.willmann@outlook.com>
2019-06-05 19:06:42 -0500

    Update upload.php

--- Beta/upload.php
+++ Beta/upload.php
@@ -1,28 +1,3 @@
 <?php
-date_default_timezone_set("America/Chicago");
-$servername = "localhost";
-$username = "u919436859_admin";
-$password = "GjefadD~i63a";
-$dbname = "u919436859_shark";
-
-$conn = new mysqli($servername, $username, $password, $dbname); // Create connection
-if ($conn->connect_error) {     // Check connection
-    die("Connection failed: " . $conn->connect_error);
-} 
-
-$Username = mysqli_real_escape_string($conn, $_GET['username']);
-$Choice = mysqli_real_escape_string($conn, $_GET['choice']);
-$Date = date("Y/m/d h:i:sa");
-
-$sql = "INSERT INTO voting (time,Username,Choice) 
-VALUES ('$Date','$Username','$Choice') ON DUPLICATE KEY UPDATE    
-time='$Date', Choice='$Choice'";
-if ($conn->query($sql) === TRUE) {
-    echo "Page saved!";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-$conn->close();
-
-header( 'Location: https://gamesharks.wizardsrwe.com/confirm.php' );
+    print_r($_POST);
 ?>
