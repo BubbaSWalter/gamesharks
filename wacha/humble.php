af97f852a59ca6d19e5394e85d21e180c3449579
BubbaSWalter <bob.willmann@outlook.com>
2019-06-05 23:24:06 -0500

    updating files for admin css and result page

--- wacha/humble.php
+++ wacha/humble.php
@@ -129,8 +129,44 @@ include '../meistercheck.php';
 					$conn->close();
 				?>
 			</div>
-		<div>
 
+			<div class="results">
+				<h4>Shark Response</h4>
+				<?php
+					$MainArray = array();
+					$conn = new mysqli($servername, $username, $password, $dbname); // Create connection
+					if ($conn->connect_error) {     // Check connection
+						die("Connection failed: " . $conn->connect_error);
+					} 
+
+					$sql = "SELECT `Choice`, COUNT(*) AS `num` FROM testvoting";
+					$result = $conn->query($sql);
+					//time,Username,Choice
+					if ($result->num_rows > 0) {
+						// output data of each row
+						while($row = $result->fetch_assoc()) {
+							echo "<h5>Total Votes: " . $row["num"] . "</h5>";
+						}
+					}
+
+					$sql = "SELECT * FROM testvoting";
+					$result = $conn->query($sql);
+					if ($result->num_rows > 0) {
+						// output data of each row
+						while($row = $result->fetch_assoc()) {
+							$MainArray[$row['Choice1']] = $MainArray[$row['Choice1']] + 1;
+							$MainArray[$row['Choice2']] = $MainArray[$row['Choice2']] + 1;
+							$MainArray[$row['Choice3']] = $MainArray[$row['Choice3']] + 1;
+						}
+					}
+					arsort($MainArray, 1);
+					foreach ($MainArray as $key => $item) {
+						echo "Challenge #" . $key . " Count: " . $item . "<br>";
+					}
+				?>
+			</div>
+			
+			
 		<div id="winners">
                 <form action="voting/winners.php">
                     Winning Choices:<br>
