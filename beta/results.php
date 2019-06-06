335e82a9fd1a7fb844076d6339c6cb0d4c0bd0e2
BubbaSWalter <bob.willmann@outlook.com>
2019-06-05 22:58:31 -0500

    Update results.php

--- beta/results.php
+++ beta/results.php
@@ -14,31 +14,11 @@
 	if ($result->num_rows > 0) {
 		// output data of each row
 		while($row = $result->fetch_assoc()) {
-            array_push($MainArray, $row['Choice1'], $row['Choice2'], $row['Choice3']);
-            //echo $row['Choice1'] . '<br>';
-            //echo $row['Choice2'] . '<br>';
-            //echo $row['Choice3'] . '<br>';
+            $MainArray[$row['Choice1']] = $MainArray[$row['Choice1']] + 1;
+            $MainArray[$row['Choice2']] = $MainArray[$row['Choice2']] + 1;
+            $MainArray[$row['Choice3']] = $MainArray[$row['Choice3']] + 1;
 		}
     }
-    $arr = array();
+    arsort($MainArray, 1);
 
-    foreach ($MainArray as $key => $item) {
-        //$arr[$item['id']][$key] = $item;
-        $arr[$item]['count'] = $arr[$item]['count'] + 1;
-        $arr[$item]['Challenge'] = $item;
-        
-    }
-
-        $trueArray = array();
-
-    foreach($arr as $item){
-        $trueArray[$item['Challenge']] = $item['count'];
-    }
-    echo "<br>";
-    echo "<br>";
-    
-    arsort($trueArray, 1);
-
-    foreach ($trueArray as $key => $item) {
-        echo "Challenge #" . $key . " Count: " . $item . "<br>";
-    }
+?>
