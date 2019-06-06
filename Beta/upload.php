01496ada16336153ee20e0816d043ac9e84ab456
BubbaSWalter <bob.willmann@outlook.com>
2019-06-05 19:34:45 -0500

    Update upload.php

--- Beta/upload.php
+++ Beta/upload.php
@@ -6,7 +6,7 @@
         $_GET['vote3'] == $_GET['vote1'] Or 
         $_GET['vote3'] == $_GET['vote2'] 
          ){
-             echo "Error: you Voted once Challenge More than Once"
+             echo "Error: you Voted once Challenge More than Once";
          }else{
             echo $_GET;
          }
