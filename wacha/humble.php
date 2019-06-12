d4bca8150d462035377a1597a3ee1cc4b1b9b67f
BubbaSWalter <bob.willmann@outlook.com>
2019-06-11 21:33:44 -0500

    updaing voting result page

--- wacha/humble.php
+++ wacha/humble.php
@@ -66,13 +66,13 @@ include '../meistercheck.php';
 			<div class="results">
 				<h4>Community Results</h4>
 				<?php
-
+					$MainArray = array();
 					$conn = new mysqli($servername, $username, $password, $dbname); // Create connection
 					if ($conn->connect_error) {     // Check connection
 						die("Connection failed: " . $conn->connect_error);
 					} 
 
-					$sql = "SELECT `Choice`, COUNT(*) AS `num` FROM voting";
+					$sql = "SELECT COUNT(*) AS `num` FROM voting";
 					$result = $conn->query($sql);
 					//time,Username,Choice
 					if ($result->num_rows > 0) {
@@ -82,18 +82,20 @@ include '../meistercheck.php';
 						}
 					}
 
-					$sql = "SELECT `Choice`, COUNT(*) AS `num` FROM voting GROUP BY `Choice`ORDER BY num DESC, Choice ASC";
+					$sql = "SELECT * FROM voting";
 					$result = $conn->query($sql);
-					//time,Username,Choice
 					if ($result->num_rows > 0) {
 						// output data of each row
 						while($row = $result->fetch_assoc()) {
-							echo "<p class='stuff'>Challenge: " . $row["Choice"] . " - Count: " . $row["num"] . "</p>";
+							$MainArray[$row['Choice1']] = $MainArray[$row['Choice1']] + 1;
+							$MainArray[$row['Choice2']] = $MainArray[$row['Choice2']] + 1;
+							$MainArray[$row['Choice3']] = $MainArray[$row['Choice3']] + 1;
 						}
-					} else {
-						echo "No Votes Submitted";
 					}
-					$conn->close();
+					arsort($MainArray, 1);
+					foreach ($MainArray as $key => $item) {
+						echo "<p class='stuff'>Challenge: #" . $key . " Count: " . $item . "<p>";
+					}
 				?>
 			</div>
 			
