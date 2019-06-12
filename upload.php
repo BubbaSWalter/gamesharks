00c68fc8ceace7385834eb6ef1b3d9b519a3c224
BubbaSWalter <bob.willmann@outlook.com>
2019-06-11 21:26:32 -0500

    Updainging voting

--- upload.php
+++ upload.php
@@ -4,25 +4,47 @@ $servername = "localhost";
 $username = "u919436859_admin";
 $password = "GjefadD~i63a";
 $dbname = "u919436859_shark";
-
+print_r($_GET)
+echo "<br>";
 $conn = new mysqli($servername, $username, $password, $dbname); // Create connection
 if ($conn->connect_error) {     // Check connection
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$Username = mysqli_real_escape_string($conn, $_GET['username']);
-$Choice = mysqli_real_escape_string($conn, $_GET['choice']);
-$Date = date("Y/m/d h:i:sa");
+if($_GET['vote1'] == $_GET['vote2'] Or 
+        $_GET['vote1'] == $_GET['vote3'] Or 
+        $_GET['vote2'] == $_GET['vote1'] Or 
+        $_GET['vote2'] == $_GET['vote3'] Or 
+        $_GET['vote3'] == $_GET['vote1'] Or 
+        $_GET['vote3'] == $_GET['vote2'] 
+         ){
+            $error = urlencode("Error: You voted one challenge more than Once");
+            header( 'Location: https://gamesharks.wizardsrwe.com/beta/voting.php?error='. $error .'&vote1='. $_GET["vote1"] . '&vote2='. $_GET["vote2"] .'&vote3='. $_GET["vote3"] );
 
-$sql = "INSERT INTO voting (time,Username,Choice) 
-VALUES ('$Date','$Username','$Choice') ON DUPLICATE KEY UPDATE    
-time='$Date', Choice='$Choice'";
-if ($conn->query($sql) === TRUE) {
-    echo "Page saved!";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-$conn->close();
+         }else{
+            $conn = new mysqli($servername, $username, $password, $dbname); // Create connection
+            if ($conn->connect_error) {     // Check connection
+                die("Connection failed: " . $conn->connect_error);
+            } else{
+                $Username = mysqli_real_escape_string($conn, $_GET['username']);
+                $Choice1 = mysqli_real_escape_string($conn, $_GET['vote1']);
+                $Choice2 = mysqli_real_escape_string($conn, $_GET['vote2']);
+                $Choice3 = mysqli_real_escape_string($conn, $_GET['vote3']);
+                $Date = date("Y/m/d h:i:sa");
 
-header( 'Location: https://gamesharks.wizardsrwe.com/confirm.php' );
+                $sql = "INSERT INTO voting (time,Username,Choice1,choice2,choice3) 
+                    VALUES ('$Date','$Username','$Choice1','$Choice2','$Choice3') ON DUPLICATE KEY UPDATE    
+                    time='$Date', Choice1='$Choice', Choice2='$Choice', Choice3='$Choice'
+                    ";
+                if ($conn->query($sql) === TRUE) {
+                    echo "Page saved!";
+                } else {
+                    echo "Error: " . $sql . "<br>" . $conn->error;
+                }
+                $conn->close();
+            }
+
+
+         }
+//header( 'Location: https://gamesharks.wizardsrwe.com/confirm.php' );
 ?>
--- voting.php
+++ voting.php
@@ -72,7 +72,7 @@
 	                if ($result->num_rows > 0) {
 		            // output data of each row
 		                while($row = $result->fetch_assoc()) {
-			                echo "<span> Current Choice:" . $row["Choice"]. "</span><br>";
+			                echo "<span> Current Choices:" . $row["Choice1"] .", " . $row["Choice2"] .", ". $row["Choice3"] .", </span><br>";
 		                }
 	                } else {
 		                echo "<span> Current Choice: NONE </span><br>";
@@ -105,23 +105,39 @@
 						</ul>
 					</div>
 					<br>
-					<select name="choice" id="choice">
 					<?php
-                		$conn = new mysqli($servername, $username, $password, $dbname); // Create connection
-                		if ($conn->connect_error) {     // Check connection
-                    		die("Connection failed: " . $conn->connect_error);
-			
-            	    	}
-            	    	$sql = "SELECT * FROM Challenges";
-            	    	$result = $conn->query($sql);
-            	    	if ($result->num_rows > 0) {
-            	    	    // output data of each row
-            	        	while($row = $result->fetch_assoc()) {
-            	    	        echo "<option value='#" . $row["ChallengeNum"] . " - " . $row["Game"] ." - " . $row["Console"] . " - " . $row["Type"] . "'>Challenge #" . $row["ChallengeNum"] . " - " . $row["Game"] ." - " . $row["Console"] . " - " . $row["Type"] . "</option>";
-            	        	}
-            	    	}
-            		?>
-					</select>
+					$sql = "SELECT * FROM Challenges";
+					$result = $conn->query($sql);
+					if ($result->num_rows > 0) {
+						// output data of each row
+						while($row = $result->fetch_assoc()) {
+							$cnum = str_replace("'", "&apos;", $row["ChallengeNum"]);
+							$game = str_replace("'", "&apos;", $row["Game"]);
+							$console = str_replace("'", "&apos;", $row["Console"]);
+							$type = str_replace("'", "&apos;", $row["Type"]);
+							$holder = $cnum . " - " . $game ." - " . $console . " - " . $type;
+							echo '	<tr>';
+							echo '		<td> Challenge#'  . $holder . '</td>';
+							if($_GET['vote1'] == $holder){
+								echo '		<td><input type="radio" name="vote1" align="center" value="' . $holder . '" checked></td>';
+							} else{
+								echo '		<td><input type="radio" name="vote1" align="center" value="' . $holder . '"></td>';
+							}
+							if($_GET['vote2'] == $holder){
+								echo '		<td><input type="radio" name="vote2" align="center" value="' . $holder . '" checked></td>';
+							} else{
+								echo '		<td><input type="radio" name="vote2" align="center" value="' . $holder . '"></td>';
+							}
+							if($_GET['vote3'] == $holder){
+								echo '		<td><input type="radio" name="vote3" align="center" value="' . $holder . '" checked></td>';
+							} else{
+								echo '		<td><input type="radio" name="vote3" align="center" value="' . $holder . '"></td>';
+							}
+							echo '	</tr>';
+
+						}
+					}
+					?>
 				<br>
 			<input type="submit" value="Vote">
 			</form>
