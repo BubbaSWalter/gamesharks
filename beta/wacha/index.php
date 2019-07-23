05a3c8ba658c4dd6d10bd4a886b0f18f4274989f
BubbaSWalter <bob.willmann@outlook.com>
2019-07-23 17:19:31 -0500

    intializing stuff

--- beta/wacha/index.php
+++ beta/wacha/index.php
@@ -18,14 +18,26 @@
 	if ($result->num_rows > 0) {
 		// output data of each row
 		while($row = $result->fetch_assoc()) {
-			$MainArray[$row['Choice1']] = $MainArray[$row['Choice1']] + 1;
+			/*$MainArray[$row['Choice1']] = $MainArray[$row['Choice1']] + 1;
 			$MainArray[$row['Choice2']] = $MainArray[$row['Choice2']] + 1;
-			$MainArray[$row['Choice3']] = $MainArray[$row['Choice3']] + 1;
+			$MainArray[$row['Choice3']] = $MainArray[$row['Choice3']] + 1;*/
+			
+			//Choice 1 Processing
+			$test = false;
+			foreach($MainArray as $item){
+				if($item['ChNum'] == $row['Choice1']){
+					echo 'Choice 1 Exsit';
+					$test = true;
+				}
+			}
+			if($test == true){
+				$MainArray['ChNum']['Count'] += 1;
+			}elseif($test == false)	{
+				$MainArray[$row['Choice1']] = array('ChNum' => $row['Choice1'], 1);
+			}
+
 		}
 	}
-	arsort($MainArray, 1);
-	foreach ($MainArray as $key => $item) {
-		echo "<p class='stuff'>Challenge: #" . $key . "-  Count: " . $item . "<p>";
-	}
+	print_r($MainArray);
 
 ?>
