a53339ef0821b827ef6bcb755cecf6bb4fe83b1f
BubbaSWalter <bob.willmann@outlook.com>
2019-07-22 22:28:25 -0500

    Fixed the Logic Order for voting upload

--- upload.php
+++ upload.php
@@ -10,37 +10,38 @@ $conn = new mysqli($servername, $username, $password, $dbname); // Create connec
 if ($conn->connect_error) {     // Check connection
     die("Connection failed: " . $conn->connect_error);
 } 
+$Choice1 = mysqli_real_escape_string($conn, $_GET['vote1']);
+$Choice2 = mysqli_real_escape_string($conn, $_GET['vote2']);
+$Choice3 = mysqli_real_escape_string($conn, $_GET['vote3']);
 
-if($_GET['vote1'] == $_GET['vote2'] Or 
-        $_GET['vote1'] == $_GET['vote3'] Or 
-        $_GET['vote2'] == $_GET['vote1'] Or 
-        $_GET['vote2'] == $_GET['vote3'] Or 
-        $_GET['vote3'] == $_GET['vote1'] Or 
-        $_GET['vote3'] == $_GET['vote2'] 
-         ){
-            $error = urlencode("Error: You voted one challenge more than Once");
+if($Choice1 == ""){
+    $Choice1 = "00 - No Vote Choice 1 -". $Username;
+}
+if($Choice2 == ""){
+    $Choice2 = "00 - No Vote Choice 2 -". $Username;
+}
+if($Choice3 == ""){
+    $Choice3 = "00 - No Vote Choice 3 -". $Username;
+}
+
+if($Choice1 == $Choice2 Or 
+    $Choice1 == $Choice3 Or 
+    $Choice2 == $Choice1 Or 
+    $Choice2 == $Choice3 Or 
+    $Choice3 == $Choice1 Or 
+    $Choice3 == $Choice2){
+    
+        $error = urlencode("Error: You voted one challenge more than Once");
             header( 'Location: https://gamesharks.wizardsrwe.com/beta/voting.php?error='. $error .'&vote1='. $_GET["vote1"] . '&vote2='. $_GET["vote2"] .'&vote3='. $_GET["vote3"] );
 
-         }else{
+        }else{
             $conn = new mysqli($servername, $username, $password, $dbname); // Create connection
             if ($conn->connect_error) {     // Check connection
                 die("Connection failed: " . $conn->connect_error);
             } else{
-
                 $Username = mysqli_real_escape_string($conn, $_GET['username']);
-                $Choice1 = mysqli_real_escape_string($conn, $_GET['vote1']);
-                $Choice2 = mysqli_real_escape_string($conn, $_GET['vote2']);
-                $Choice3 = mysqli_real_escape_string($conn, $_GET['vote3']);
                 $Date = date("Y/m/d h:i:sa");
-                if($Choice1 == ""){
-                    $Choice1 = "00 - No Vote Choice 1 -". $Username;
-                }
-                if($Choice2 == ""){
-                    $Choice2 = "00 - No Vote Choice 2 -". $Username;
-                }
-                if($Choice3 == ""){
-                    $Choice3 = "00 - No Vote Choice 3 -". $Username;
-                }
+
 
                 $sql = "INSERT INTO voting (time,Username,Choice1,Choice2,Choice3) 
                     VALUES ('$Date','$Username','$Choice1','$Choice2','$Choice3') ON DUPLICATE KEY UPDATE    
