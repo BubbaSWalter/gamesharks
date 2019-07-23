366453a7402799b8f5c5c617bb284a9a1760ac47
BubbaSWalter <bob.willmann@outlook.com>
2019-07-23 15:40:26 -0500

    Final Test hopefully

--- beta/index.php
+++ beta/index.php
@@ -2,7 +2,11 @@
 <?php
 	$MainArray = array();
 	array_push($MainArray,array("ChNum"=> 1, "Count"=>"12"),array("ChNum"=> 2, "Count"=>"12"),array("ChNum"=> 3, "Count"=>"42"));
-
+	$x = 0;
+	while($x <=100){
+		array_push($MainArray,array("ChNum"=> $x, "Count"=>"12"));
+		$x++;
+	}
 	/*var $MainArray = array();
 	while($x <= 100) {
 		var $temparray = array(
