5bdac07a096feb2c875d81fae2ade70911c5a6dd
BubbaSWalter <bob.willmann@outlook.com>
2019-07-23 18:41:25 -0500

    test

--- wacha/humble.php
+++ wacha/humble.php
@@ -65,7 +65,37 @@ include '../meistercheck.php';
 			
 			<div class="results">
 				<h4>Community Results</h4>
-				
+				<?php
+					$MainArray = array();
+					$conn = new mysqli($servername, $username, $password, $dbname); // Create connection
+					if ($conn->connect_error) {     // Check connection
+						die("Connection failed: " . $conn->connect_error);
+					} 
+
+					$sql = "SELECT COUNT(*) AS `num` FROM voting";
+					$result = $conn->query($sql);
+					//time,Username,Choice
+					if ($result->num_rows > 0) {
+						// output data of each row
+						while($row = $result->fetch_assoc()) {
+							echo "<h5>Total Votes: " . $row["num"] . "</h5>";
+						}
+					}
+
+					$sql = "SELECT * FROM voting";
+					$result = $conn->query($sql);
+					if ($result->num_rows > 0) {
+						// output data of each row
+						while($row = $result->fetch_assoc()) {
+							$MainArray[$row['Choice1']] = $MainArray[$row['Choice1']] + 1;
+							$MainArray[$row['Choice2']] = $MainArray[$row['Choice2']] + 1;
+							$MainArray[$row['Choice3']] = $MainArray[$row['Choice3']] + 1;
+						}
+					}
+					arsort($MainArray, 1);
+					foreach ($MainArray as $key => $item) {
+						echo "<p class='stuff'>Challenge: #" . $key . "-  Count: " . $item . "<p>";
+				?>
 			</div>
 			
 		<div id="winners">
