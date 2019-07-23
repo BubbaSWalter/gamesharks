9db329466c83e71646a23d2cbfa62ead271b6e8e
BubbaSWalter <bob.willmann@outlook.com>
2019-07-23 14:15:06 -0500

    te

--- beta/wacha/humble.php
+++ beta/wacha/humble.php
@@ -87,12 +87,13 @@ include '../../meistercheck.php';
 					if ($result->num_rows > 0) {
 						// output data of each row
 						while($row = $result->fetch_assoc()) {
+							print_r($row['Choice1']);
 							$MainArray[$row['Choice1']] = $MainArray[$row['Choice1']] + 1;
 							$MainArray[$row['Choice2']] = $MainArray[$row['Choice2']] + 1;
 							$MainArray[$row['Choice3']] = $MainArray[$row['Choice3']] + 1;
 						}
 					}
-					print_r($MainArray);
+
 					arsort($MainArray, 1);
 					foreach ($MainArray as $key => $item) {
 						echo "<p class='stuff'>Challenge: #" . $key . "-  Count: " . $item . "<p>";
