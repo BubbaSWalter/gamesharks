0eef244b1d2ed6025ad59e5a137b20bc7d1f0553
BubbaSWalter <bob.willmann@outlook.com>
2019-07-23 17:12:14 -0500

    Test

--- beta/wacha/index.php
+++ beta/wacha/index.php
@@ -2,10 +2,21 @@
 <?php
 	$MainArray = array();
 	include '../mesitercheck.php';
+	$MainArray = array();
 	$conn = new mysqli($servername, $username, $password, $dbname); // Create connection
 	if ($conn->connect_error) {     // Check connection
 		die("Connection failed: " . $conn->connect_error);
 	} 
+
+	$sql = "SELECT COUNT(*) AS `num` FROM voting";
+	$result = $conn->query($sql);
+	//time,Username,Choice
+	if ($result->num_rows > 0) {
+		// output data of each row
+		while($row = $result->fetch_assoc()) {
+			echo "<h5>Total Votes: " . $row["num"] . "</h5>";
+		}
+	}
 	$sql = "SELECT * FROM voting";
 	$result = $conn->query($sql);
 	if ($result->num_rows > 0) {
