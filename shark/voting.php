5b6088bbc9eac421794802dc8fc3d3e559edd892
BubbaSWalter <bob.willmann@outlook.com>
2020-01-23 12:18:14 -0600

    Updated Shark Design

--- shark/voting.php
+++ shark/voting.php
@@ -38,104 +38,66 @@
 	<link rel="stylesheet" type="text/css" href="/css/navbar.css">
 	<link rel="stylesheet" type="text/css" href="/css/header.css">
 	<link rel="stylesheet" type="text/css" href="/css/main.css">
-	<link rel="stylesheet" type="text/css" href="/css/lists.css">
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <body>
     
     <canvas id='bgCanvas'></canvas>
-        <div id="Sharky" class="center">
-	        <img src="/img/GameSharks_logo_square.png" alt="GameSharks"  width="369" height="200" class="center">
-		    <h1>GameSharks Offical Shark Ballot</h1>
-		    <h1>Shark Only</h1>
-	    </div>
-	    <article id="article">
-		    <p>You are allowed to vote once.  If you want to change your vote please ping BubbaSWalter on Discord. Multiple votes could result in votes being voided</p>
-		    Person Voting:<span id="username"><?php echo $uname; ?></span><br>
-			<form action="upload.php">
-				<input type="hidden" name="username" value="<?php echo $uname; ?>">
-				Pick Your Poison:<br>
-				<button type="button" onclick="list()" >Show/Hide The List</button>
-				<div id="list">
-					<ul>
-						<?php
-                    		$conn = new mysqli($servername, $username, $password, $dbname);
-                    		if ($conn->connect_error) {     // Check connection
-	                    		die("Connection failed: " . $conn->connect_error);
-	                		}
-	                		$winners = array();
-	                		$sql = "SELECT * FROM winner";
-	                		$result = $conn->query($sql);
-	                		if ($result->num_rows > 0) {
-                        		// output data of each row
-                        		while($row = $result->fetch_assoc()) {
-                            		$winners = $row;
-                        		}} else {
-                            		echo "0 results";
-                        		}
-                    		$sql = "SELECT * FROM `Challenges` WHERE `ChallengeNum`='" . $winners['first'] . "' or `ChallengeNum`='" . $winners['second'] . "' or `ChallengeNum`='" . $winners['third'] . "' or `ChallengeNum`='" . $winners['fourth'] . "' or `ChallengeNum`='" . $winners['fifth'] . "'  ORDER BY ChallengeNum ASC";
-                    		$result = $conn->query($sql);
-                    		if ($result->num_rows > 0) {
-                        		while($row = $result->fetch_assoc()) {
-                            		echo "<li><p>Challenge #" . $row["ChallengeNum"] . " - " . $row["Game"] ." - " . $row["Console"] . " - " . $row["Type"]. "</p>";
-			                		echo "<p>". $row["Description"] . "</p>";
-			                		if(!is_null ( $row["SaveState"] )){
-			                		    echo "<p>" . $row["SaveState"] . "</p>" ;
-			                		}
-			                		echo "</li>";
-                    		}} else {
-                        		echo "0 results";
-                    		}
-                    		$conn->close();
-						?>
-					</ul>
-				</div>
+    <div id="Sharky" class="center">
+	    <img src="/img/GameSharks_logo_square.png" alt="GameSharks"  width="369" height="200" class="center">
+	    <h1>GameSharks Offical Shark Ballot</h1>
+	    <h1>Shark Only</h1>
+	</div>
+	<article id="article">
+	    <p>You are allowed to vote once.  If you want to change your vote please ping BubbaSWalter on Discord. Multiple votes could result in votes being voided</p>
+		Person Voting:<span id="username"><?php echo $uname; ?></span><br>
+		<form action="upload.php">
+			<input type="hidden" name="username" value="<?php echo $uname; ?>">
 
-				<br>
-				<table>
-  					<tr>
-    					<th>Challenge</th>
-    					<th>Choice</th> 
-  					</tr>
-					<?php
-						$conn = new mysqli($servername, $username, $password, $dbname);
-						if ($conn->connect_error) {     // Check connection
-							die("Connection failed: " . $conn->connect_error);
-						}
-						$winners = array();
-	                	$sql = "SELECT * FROM winner";
-	                	$result = $conn->query($sql);
-	                	if ($result->num_rows > 0) {
-                        	// output data of each row
-                        	while($row = $result->fetch_assoc()) {
-                            	$winners = $row;
-                        }} else {
-                        	echo "0 results";
-						}
-						
-                    	$sql = "SELECT * FROM `Challenges` WHERE `ChallengeNum`='" . $winners['first'] . "' or `ChallengeNum`='" . $winners['second'] . "' or `ChallengeNum`='" . $winners['third'] . "' or `ChallengeNum`='" . $winners['fourth'] . "' or `ChallengeNum`='" . $winners['fifth'] . "'  ORDER BY ChallengeNum ASC";
-                    	$result = $conn->query($sql);
-                    	if ($result->num_rows > 0) {
-                        	while($row = $result->fetch_assoc()) {
-								$cnum = str_replace("'", "&apos;", $row["ChallengeNum"]);
-								$game = str_replace("'", "&apos;", $row["Game"]);
-								$console = str_replace("'", "&apos;", $row["Console"]);
-								$type = str_replace("'", "&apos;", $row["Type"]);
-								$holder = $cnum . " - " . $game ." - " . $console . " - " . $type;
-								echo "<tr>";
-									echo "<td>$holder</td>";
-									echo '<td><input type="radio" name="choice" value="' . $holder . '"></td>';
-								echo "</tr>";
-                    	}} else {
-                        	echo "0 results";
-                    	}
-						$conn->close();
-						
-					?>
-  					
-				</table>
-				<br>
-  				<input type="submit" value="Vote">
-			</form>
+			<?php
+				$conn = new mysqli($servername, $username, $password, $dbname);
+				if ($conn->connect_error) {     // Check connection
+					die("Connection failed: " . $conn->connect_error);
+				}
+				$winners = array();
+				$sql = "SELECT * FROM winner";
+				$result = $conn->query($sql);
+				if ($result->num_rows > 0) {
+					// output data of each row
+					while($row = $result->fetch_assoc()) {
+						$winners = $row;
+					}
+				    
+				} else {
+					echo "0 results";
+					
+				}
+				$sql = "SELECT * FROM `Challenges` WHERE `ChallengeNum`='" . $winners['first'] . "' or `ChallengeNum`='" . $winners['second'] . "' or `ChallengeNum`='" . $winners['third'] . "' or `ChallengeNum`='" . $winners['fourth'] . "' or `ChallengeNum`='" . $winners['fifth'] . "'  ORDER BY ChallengeNum ASC";
+				$result = $conn->query($sql);
+				if ($result->num_rows > 0) {
+                    while($row = $result->fetch_assoc()) {
+						echo "<h3>Challenge #" . $row["ChallengeNum"] . "</h3>";
+						echo "<ul>";
+						echo "   <li>" . $row["Game"] ." - " . $row["Console"] . " - " . $row["Type"] . "</li>";
+						echo "   <li>" . "Challenge Description </li>";
+						echo "   <li>" . $row["Description"] . "</li>";
+						echo "</ul>";
+						$cnum = str_replace("'", "&apos;", $row["ChallengeNum"]);
+						$game = str_replace("'", "&apos;", $row["Game"]);
+						$console = str_replace("'", "&apos;", $row["Console"]);
+						$type = str_replace("'", "&apos;", $row["Type"]);
+						$holder = $cnum . " - " . $game ." - " . $console . " - " . $type;
+						echo 'Vote for this challenge: <input type="radio" name="choice" value="' . $holder . '"></br>';
+						echo '</br>';
+					}
+				} else {
+                    echo "0 results";
+                }
+                $conn->close();
+
+			?>
+			<input type="submit" value="Vote">
+		</form>
+
 		
 		
 	</article>
