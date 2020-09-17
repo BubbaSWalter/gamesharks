c6e62704c42663c0b93cc5ff0540d4441ba25644
BubbaSWalter <bob.willmann@outlook.com>
2020-09-17 18:32:59 -0500

    Update humble.php

--- wacha/humble.php
+++ wacha/humble.php
@@ -36,7 +36,7 @@ include '../meistercheck.php';
 	<meta charset="UTF-8">
 	<title>GameSharks Results Page</title>
 	<link rel="stylesheet" type="text/css" href="/css/header.css">
-	<link rel="stylesheet" type="text/css" href="/css/main.css">
+	<!--<link rel="stylesheet" type="text/css" href="/css/main.css"> -->
 	<link rel="stylesheet" type="text/css" href="/css/admin.css">
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <body>
