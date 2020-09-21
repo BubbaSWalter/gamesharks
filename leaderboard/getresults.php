661e24fb0019af1817ade8c9b5c104b379c0013c
BubbaSWalter <bob.willmann@outlook.com>
2020-09-21 09:30:46 -0500

    Update getresults.php

--- leaderboard/getresults.php
+++ leaderboard/getresults.php
@@ -19,7 +19,7 @@
         if ($result->num_rows > 0) {
 		    // output data of each row
 		    while($row = $result->fetch_assoc()) {
-                $dumb = [$row['User_Name'], $row['Challlenge_Num'], $row['Challenge_Score'], $row['Challenge_Points'], ]
+                $dumb = [$row['User_Name'], $row['Challlenge_Num'], $row['Challenge_Score'], $row['Challenge_Points'] ];
                 array_push ( $holder, $dumb  );
 		    }
         }
