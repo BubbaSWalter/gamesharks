fc82a181f1813770dd9b3620de1a8dba4e6c1194
BubbaSWalter <bob.willmann@outlook.com>
2020-09-21 12:06:35 -0500

    Update tsgetresults.php

--- leaderboard/tsgetresults.php
+++ leaderboard/tsgetresults.php
@@ -25,7 +25,7 @@
         if ($result->num_rows > 0) {
             // output data of each row
             while($row = $result->fetch_assoc()) {
-                print_r($row);
+                #print_r($row);
                 $Ch1 = $row['Challenge 1'];
             
                 if ($Ch1 == NULL) {
