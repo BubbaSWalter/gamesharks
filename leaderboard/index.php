a6f4fe6cc8245448e5c86d03924961edd1938e37
BubbaSWalter <bob.willmann@outlook.com>
2020-09-21 03:54:49 -0500

    Update index.php

--- leaderboard/index.php
+++ leaderboard/index.php
@@ -9,7 +9,7 @@
 	<link rel="stylesheet" type="text/css" href="/css/header.css">
 	<link rel="stylesheet" type="text/css" href="/css/main.css">
 	<link rel="stylesheet" type="text/css" href="/css/iframe.css">
-	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <body>
     <canvas id='bgCanvas'></canvas>
 	<div id="Sharky" class="center">
@@ -21,5 +21,11 @@
 	</article>
 	
 <?php include 'include/footer.php'?>
+<script>
+	$( document ).ready(function() {
+		$('#dado').contents().find('body').css('background', '-webkit-radial-gradient(circle, #1a82f7, #2F2727);';
+	});
+	
+</script>
 </body>
 </html>
