69a638fa19ad8d7ccec3eba5afbf474b460ce75b
BubbaSWalter <bob.willmann@outlook.com>
2020-09-21 13:56:05 -0500

    Update gupvoting.php

--- wacha/gupvoting.php
+++ wacha/gupvoting.php
@@ -3,7 +3,9 @@
 	$username = "u919436859_admin";
 	$password = "GjefadD~i63a";
 	$dbname = "u919436859_shark";
-	$MainArray = array();
+    $MainArray = [];
+    
+
 	$conn = new mysqli($servername, $username, $password, $dbname); // Create connection
 	if ($conn->connect_error) {     // Check connection
 		die("Connection failed: " . $conn->connect_error);
@@ -15,63 +17,20 @@
     //time,Username,Choice
 	if ($result->num_rows > 0) {
 		// output data of each row
-						while($row = $result->fetch_assoc()) {
-							echo "<h5>Total Votes: " . $row["num"] . "</h5>";
-						}
-					}
-
-					$sql = "SELECT * FROM voting";
-					$result = $conn->query($sql);
-					if ($result->num_rows > 0) {
-						// output data of each row
-						while($row = $result->fetch_assoc()) {
-							//Choice 1 Processing
-							$test = false;
-							foreach($MainArray as $item){
-								if($item['ChNum'] == $row['Choice1']){
-									$test = true;
-								}
-							}
-							if($test == true){
-								$MainArray[$row['Choice1']]['Count'] += 1;
-							}elseif($test == false)	{
-								$MainArray[$row['Choice1']] = array('ChNum' => $row['Choice1'],'Count' => 1);
-							}
-
-							//Choice 2 Processing
-							$test = false;
-							foreach($MainArray as $item){
-								if($item['ChNum'] == $row['Choice2'])
-								{
-									$test = true;
-								}
-							}
-							if($test == true){
-								$MainArray[$row['Choice2']]['Count'] += 1;
-							}elseif($test == false)	{
-								$MainArray[$row['Choice2']] = array('ChNum' => $row['Choice2'], 'Count' => 1);
-							}
-
-
-							//Choice 3 Processing
-							$test = false;
-							foreach($MainArray as $item){
-								if($item['ChNum'] == $row['Choice3'])
-								{
-									$test = true;
-								}
-							}
-							if($test == true){
-								$MainArray[$row['Choice3']]['Count'] += 1;
-							}elseif($test == false)	{
-								$MainArray[$row['Choice3']] = array('ChNum' => $row['Choice3'],'Count' => 1);
-							}
-						}
-					}
-					$ChNum = array_column($MainArray,'ChNum');
-					$Count = array_column($MainArray, 'Count');
-					array_multisort($Count, SORT_DESC, $ChNum,SORT_ASC, $MainArray);
-					foreach ($MainArray as $key => $value) {
-						echo '<p>Challenge #' .$value['ChNum'] . ' - ' . $value['Count'] . '</p>';
-					}
-				?>
+		while($row = $result->fetch_assoc()) {
+			echo "<h5>Total Votes: " . $row["num"] . "</h5>";
+		}
+    }
+    
+    $sql = "SELECT * AS `num` FROM voting";
+	if ($result->num_rows > 0) {
+		// output data of each row
+		while($row = $result->fetch_assoc()) {
+            $MainArray[$Row['Choice1']] +=  1;
+            $MainArray[$Row['Choice2']] +=  1;
+            $MainArray[$Row['Choice3']] +=  1;
+		}
+    }
+    echo(json_encode($trueholder));
+    
+?>
