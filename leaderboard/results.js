d7a9980fd9ca76e5ce69a640dc07624ec0016b61
BubbaSWalter <bob.willmann@outlook.com>
2020-09-21 11:03:43 -0500

    Update results.js

--- leaderboard/results.js
+++ leaderboard/results.js
@@ -12,13 +12,19 @@ function LoadChallenge() {
     url = "pcgetresults.php?db=" + DataBase + "&ch=" + Challenge;
     alert("pcgetresults.php?db=" + DataBase + "&ch=" + Challenge);
 
+/*     <td id='pc1name' class="tg-gri2"></td>
+    <td id='pc1score' class="tg-gri2"></td>
+    <td id='pc1point' class="tg-gri2"></td> */
+
+
     $.get( url , function( data ) {
         $( ".result" ).html( data );
         alert( data );
         $holder = JSON.parse(data)
         console.log($holder);
+        count = 1
         $holder.forEach(element => {
-            console.log(element);
+            $("#pc" + count + "name").text(element[0]);
         });
       });
 
