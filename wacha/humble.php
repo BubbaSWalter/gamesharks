991ea97d45b4eefa9df68652d781feff885fb3e8
BubbaSWalter <bob.willmann@outlook.com>
2020-09-21 13:38:14 -0500

    Update humble.php

--- wacha/humble.php
+++ wacha/humble.php
@@ -46,10 +46,10 @@ include '../meistercheck.php';
 		<h1>Voting Results</h1>
 		<div id="navbar">
     <ul>
-        <li><a href="humble.php">Results</a></li>
-        <li><a href="user.php">User Management</a></li>
-        <li><a href="challenges.php">Challenge List</a></li>
-        <li><a href="sharkmas.php">Sharkmas List</a></li>
+        <li><a href="/wacha/humble.php">Results</a></li>
+        <li><a href="/wacha/user.php">User Management</a></li>
+        <li><a href="/wacha/challenges.php">Challenge List</a></li>
+        <li><a href="/wacha/sharkmas.php">Sharkmas List</a></li>
         <li><a href="/">Exit Admin Area</a></li>
     </ul>
 </div>
@@ -80,112 +80,9 @@ include '../meistercheck.php';
 	<article id="article">
 		<div id="adminmain">
 			
-			<div class="results">
-				<h4>Community Results</h4>
-				<?php
-					$MainArray = array();
-					$conn = new mysqli($servername, $username, $password, $dbname); // Create connection
-					if ($conn->connect_error) {     // Check connection
-						die("Connection failed: " . $conn->connect_error);
-					} 
-
-					$sql = "SELECT COUNT(*) AS `num` FROM voting";
-					$result = $conn->query($sql);
-					//time,Username,Choice
-					if ($result->num_rows > 0) {
-						// output data of each row
-						while($row = $result->fetch_assoc()) {
-							echo "<h5>Total Votes: " . $row["num"] . "</h5>";
-						}
-					}
-
-					$sql = "SELECT * FROM voting";
-					$result = $conn->query($sql);
-					if ($result->num_rows > 0) {
-						// output data of each row
-						while($row = $result->fetch_assoc()) {
-							//Choice 1 Processing
-							$test = false;
-							foreach($MainArray as $item){
-								if($item['ChNum'] == $row['Choice1']){
-									$test = true;
-								}
-							}
-							if($test == true){
-								$MainArray[$row['Choice1']]['Count'] += 1;
-							}elseif($test == false)	{
-								$MainArray[$row['Choice1']] = array('ChNum' => $row['Choice1'],'Count' => 1);
-							}
-
-							//Choice 2 Processing
-							$test = false;
-							foreach($MainArray as $item){
-								if($item['ChNum'] == $row['Choice2'])
-								{
-									$test = true;
-								}
-							}
-							if($test == true){
-								$MainArray[$row['Choice2']]['Count'] += 1;
-							}elseif($test == false)	{
-								$MainArray[$row['Choice2']] = array('ChNum' => $row['Choice2'], 'Count' => 1);
-							}
 
 
-							//Choice 3 Processing
-							$test = false;
-							foreach($MainArray as $item){
-								if($item['ChNum'] == $row['Choice3'])
-								{
-									$test = true;
-								}
-							}
-							if($test == true){
-								$MainArray[$row['Choice3']]['Count'] += 1;
-							}elseif($test == false)	{
-								$MainArray[$row['Choice3']] = array('ChNum' => $row['Choice3'],'Count' => 1);
-							}
-						}
-					}
-					$ChNum = array_column($MainArray,'ChNum');
-					$Count = array_column($MainArray, 'Count');
-					array_multisort($Count, SORT_DESC, $ChNum,SORT_ASC, $MainArray);
-					foreach ($MainArray as $key => $value) {
-						echo '<p>Challenge #' .$value['ChNum'] . ' - ' . $value['Count'] . '</p>';
-					}
-				?>
-			</div>
 
-			<h4>Shark Response</h4>
-				<?php
-					$conn = new mysqli($servername, $username, $password, $dbname); // Create connection
-					if ($conn->connect_error) {     // Check connection
-						die("Connection failed: " . $conn->connect_error);
-					} 
-
-					$sql = "SELECT `Choice`, COUNT(*) AS `num` FROM sharkvoting";
-					$result = $conn->query($sql);
-					//time,Username,Choice
-					if ($result->num_rows > 0) {
-						// output data of each row
-						while($row = $result->fetch_assoc()) {
-							echo "<h5>Total Votes: " . $row["num"] . "</h5>";
-						}
-					}
-
-					$sql = "SELECT `Choice`, COUNT(*) AS `num` FROM sharkvoting GROUP BY `Choice`ORDER BY num DESC, Choice ASC";
-					$result = $conn->query($sql);
-					//time,Username,Choice
-					if ($result->num_rows > 0) {
-						// output data of each row
-						while($row = $result->fetch_assoc()) {
-							echo "<p class='stuff'>Challenge: " . $row["Choice"] . " - Count: " . $row["num"] . "</p>";
-						}
-					} else {
-						echo "No Votes Submitted";
-					}
-					$conn->close();
-				?>
 		<div class="results">
 				<h4>Shark Response</h4>
 				<?php
