6e2465d7eb0d3d6aff7121faa123438f957a3298
BubbaSWalter <bob.willmann@outlook.com>
2020-09-21 14:02:14 -0500

    Update gupvoting.php

--- wacha/gupvoting.php
+++ wacha/gupvoting.php
@@ -21,13 +21,10 @@
 			echo "<h5>Total Votes: " . $row["num"] . "</h5>";
 		}
     }
-    $conn->close();
-    $conn = new mysqli($servername, $username, $password, $dbname); // Create connection
-	if ($conn->connect_error) {     // Check connection
-		die("Connection failed: " . $conn->connect_error);
-	} 
+
 
     $sql = "SELECT * FROM voting";
+    $result = $conn->query($sql);
 	if ($result->num_rows > 0) {
 		// output data of each row
 		while($row = $result->fetch_assoc()) {
