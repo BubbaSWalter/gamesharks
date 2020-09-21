75d8a5213fe6e6b5c6b75bf0c74c046bcbc72d91
BubbaSWalter <bob.willmann@outlook.com>
2020-09-21 09:30:06 -0500

    Update getresults.php

--- leaderboard/getresults.php
+++ leaderboard/getresults.php
@@ -4,7 +4,25 @@
     $password = "GjefadD~i63a";
     $dbname = "u919436859_shark";
 
-    $database = $_POST['db'];
-
-    print $database;
+    #$database = $_POST['db'];
+    $database = 'Guppy_ScoreBoard';
+    $holder = []
+    #print $database;
+        $conn = new mysqli($servername, $username, $password, $dbname); // Create connection
+		if ($conn->connect_error) {     // Check connection
+			die("Connection failed: " . $conn->connect_error);
+		}
+		
+		$sql = "SELECT * FROM $database ";
+        $result = $conn->query($sql);
+        
+        if ($result->num_rows > 0) {
+		    // output data of each row
+		    while($row = $result->fetch_assoc()) {
+                $dumb = [$row['User_Name'], $row['Challlenge_Num'], $row['Challenge_Score'], $row['Challenge_Points'], ]
+                array_push ( $holder, $dumb  );
+		    }
+        }
+        print_r($holder);
+	    $conn->close();
 ?>
