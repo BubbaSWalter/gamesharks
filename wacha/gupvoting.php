45e821704ce7a407ac14e45376d09ae07d2e041e
BubbaSWalter <bob.willmann@outlook.com>
2020-09-21 13:58:44 -0500

    Update gupvoting.php

--- wacha/gupvoting.php
+++ wacha/gupvoting.php
@@ -26,6 +26,7 @@
 	if ($result->num_rows > 0) {
 		// output data of each row
 		while($row = $result->fetch_assoc()) {
+            print_r($row);
             $MainArray[$Row['Choice1']] +=  1;
             $MainArray[$Row['Choice2']] +=  1;
             $MainArray[$Row['Choice3']] +=  1;
