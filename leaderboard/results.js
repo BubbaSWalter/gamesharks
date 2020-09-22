7ae685a2dfcdcd1b0ed918311f60d7d68e93cdbc
BubbaSWalter <bob.willmann@outlook.com>
2020-09-21 21:10:00 -0500

    Update results.js

--- leaderboard/results.js
+++ leaderboard/results.js
@@ -55,8 +55,14 @@ function LoadChallenge() {
         $holder.forEach(element => {
             $("#pc" + count + "name").text(element[0]);
             if (element[1] + " ( " + element[2] + " )" != "null ( null )"){
-                $("#pc" + count + "score").text(element[1] + " ( " + element[2] + " )");
-            } else{
+                console.log(element[2].substring(0, 2));
+                if(element[2].substring(0, 2)== '00'){
+                    $("#pc" + count + "score").text(element[1] + " ( " + element[2].substring(3) + " )");
+                } else {
+                    $("#pc" + count + "score").text(element[1] + " ( " + element[2] + " )");
+                }
+                
+            }} else{
                 $("#pc" + count + "score").text("");
             }
             $("#pc" + count + "point").text(element[3]);
