5c1fe4f948c38230753899dcbc7b92fef941b4ba
BubbaSWalter <bob.willmann@outlook.com>
2020-09-21 20:11:30 -0500

    Update header.php

--- wacha/include/header.php
+++ wacha/include/header.php
@@ -2,9 +2,9 @@
 <html>
 	<meta charset="UTF-8">
 	<title>GameSharks Results Page</title>
-	<link rel="stylesheet" type="text/css" href="../css/header.css">
-	<link rel="stylesheet" type="text/css" href="../css/main.css">
-	<link rel="stylesheet" type="text/css" href="../css/admin.css">
+	<link rel="stylesheet" type="text/css" href="/css/header.css">
+	<link rel="stylesheet" type="text/css" href="/css/main.css">
+	<link rel="stylesheet" type="text/css" href="/css/admin.css">
 	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <body>
     <canvas id='bgCanvas'></canvas>
