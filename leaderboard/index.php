f203806b1c3b037df179dd9655a84e690b80d7b5
BubbaSWalter <bob.willmann@outlook.com>
2020-10-06 17:52:01 -0500

    Update index.php

--- leaderboard/index.php
+++ leaderboard/index.php
@@ -259,6 +259,7 @@
   				</tr>
 			</tbody>
 		</table>
+			<p>If your Score end in an "*" you either started you timer early or late and The Mesiter Team Adjusted your time accordingly</p> 
 		</div>
 		</div>
 
