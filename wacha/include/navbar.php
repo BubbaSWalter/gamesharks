830d0e94d91dfa0bf7e1309e2e9e3aaca4ac7ffa
BubbaSWalter <bob.willmann@outlook.com>
2020-10-05 19:59:57 -0500

    Update navbar.php

--- wacha/include/navbar.php
+++ wacha/include/navbar.php
@@ -1,9 +1,9 @@
 <div id="navbar">
     <ul>
-        <li><a href="humble.php">Results</a></li>
-        <li><a href="user.php">User Management</a></li>
-        <li><a href="challenges.php">Challenge List</a></li>
-        <li><a href="sharkmas.php">Sharkmas List</a></li>
+        <li><a href="/wacha/humble.php">Results</a></li>
+        <li><a href="/wacha/user.php">User Management</a></li>
+        <li><a href="/wacha/challenges.php">Challenge List</a></li>
+        <li><a href="/wacha/sharkmas.php">Sharkmas List</a></li>
         <li><a href="/">Exit Admin Area</a></li>
     </ul>
 </div>
