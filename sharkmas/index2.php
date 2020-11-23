52bdca002e6d6eee7e6a1979481de9ca1aaf98e7
BubbaSWalter <bob.willmann@outlook.com>
2020-11-23 02:13:42 -0600

    Update index2.php

--- sharkmas/index2.php
+++ sharkmas/index2.php
@@ -60,7 +60,7 @@
   <div class="snow__flake"></div>
 </div>
 	<div id="Sharky" class="center">
-	<img src="img/GameSharks_Sharkmas_logo.png" alt="GameSharks" height="300" class="center" style="object-fit: cover;">
+	<img src="/img/GameSharks_Sharkmas_logo.png" alt="GameSharks" height="300" class="center" style="object-fit: cover;">
 		<h1>GameSharks Validation Area</h1>
 	</div>
 	<article id="article">
