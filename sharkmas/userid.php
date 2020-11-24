356fcdd14f643276ca17589004870b9ab42cae15
BubbaSWalter <bob.willmann@outlook.com>
2020-11-24 12:54:16 -0600

    Update userid.php

--- sharkmas/userid.php
+++ sharkmas/userid.php
@@ -1,11 +1,11 @@
 <?php
     if(isset($_COOKIE["username"])) {
+        echo '<br>';
         echo 'Your UserName: ' . $uname . '</br>';
-        echo 'Your UserID: ' . $uid . '</br>';
         echo 'If the above line says "GUEST" and "0" contact Bubba before proceeding.';
     } else {
+        echo '<br>';
         echo 'Your UserName:  GUEST </br>';
-        echo 'Your UserID: 0 </br>';
         echo 'If the above line says "GUEST" and "0" or their are blank contact Bubba before proceeding.';
     }
 
