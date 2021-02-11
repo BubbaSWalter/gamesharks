4d1f6670ad4865e51927640bb7bdb939f57542da
BubbaSWalter <bob.willmann@outlook.com>
2021-02-10 20:05:09 -0600

    some code optiomzations

--- leaderboard/test.js
+++ leaderboard/test.js
@@ -5,6 +5,4 @@ $.post( "chlist.php", function( data ) {
     for (i = 0; i < obj.length; i++) {
         $("#challenge").append('<option value=' & obj[0] &'>Challenge '& obj[0] & obj[1]&' - '& obj[2]&' - ' & obj[3]& '</option>');
     }
-
-
   });
